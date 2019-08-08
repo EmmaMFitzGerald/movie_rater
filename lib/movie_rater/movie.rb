d418701ac3943e604f368fe65f4a84c65bf494e2
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-08 00:45:56 +0000

    required open-uri

--- lib/movie_rater/movie.rb
+++ lib/movie_rater/movie.rb
@@ -22,7 +22,11 @@ class MovieRater::Movie
     # movie_2.genre = "Crime, Drama"
     # movie_2.rating = "9.2"
     
-    [movie_1, movie_2]
+    # [movie_1, movie_2]
+  end
+  
+  def self.scrape_imdb
+    doc = Nokogiri::HTML(open("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating"))
   end
   
 end
